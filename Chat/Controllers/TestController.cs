ec475cdc890dd83bb1cd723fe3c6b217b5537fb8
Антон Антон <justwant6@mail.ru>
2024-02-09 18:42:03 +0300

    feat: добавит проект для работы с бд

--- Chat/Controllers/TestController.cs
+++ Chat/Controllers/TestController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Chat.Data.Database;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Chat.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class TestController : ControllerBase
     {
-        public string Test (string test)
+        public string Test ([FromServices] DBContext dBContext, string test)
         {
+            var ttt = dBContext.Users.ToArray();
+
             return test;
         }
     }
--- Chat/Program.cs
+++ Chat/Program.cs
@@ -1,22 +1,12 @@
+using Chat.Data.Database;
 using Chat.Hubs;
+using LinqToDB;
+using LinqToDB.AspNet;
+using LinqToDB.AspNet.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerDocument();
-builder.Services.AddSignalR();
-builder.Services.AddCors(option =>
-{
-    option.AddPolicy(
-        "cors",
-        p =>
-        p
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowAnyOrigin());
-});
+ConfigureServices(builder);
 
 var app = builder.Build();
 
@@ -37,3 +27,28 @@ app.ConfigureHubs();
 app.MapControllers();
 
 app.Run();
+
+
+static void ConfigureServices (IHostApplicationBuilder builder)
+{
+    builder.Services.AddLinqToDBContext<DBContext>((provider, options) =>
+        options
+            .UsePostgreSQL(builder.Configuration.GetConnectionString("Default")!)
+            .UseDefaultLogging(provider));
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerDocument();
+    builder.Services.AddSignalR();
+    builder.Services.AddCors(option =>
+    {
+        option.AddPolicy(
+            "cors",
+            p =>
+            p
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin());
+    });
+}
