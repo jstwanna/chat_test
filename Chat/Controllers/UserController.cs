011005116a0fd9b9559d0ef87d31287106ad209f
Антон Антон <justwant6@mail.ru>
2024-02-22 23:57:32 +0300

    feat: Добавит user controller

---
+++ Chat/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController ()
+        {
+
+        }
+
+        /// <summary>
+        /// Получить информацию о пользователе
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult GetUserInfo ()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Изменить данные пользователя
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult ChangeUserData ()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Изменить пароль пользователя
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult ChangeUserPassword ()
+        {
+            return Ok();
+        }
+    }
+}
